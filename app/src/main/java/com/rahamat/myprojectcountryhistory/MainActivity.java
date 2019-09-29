d39e32b98800a84a502b00cc927168edc2ed700c
urahamat01 <urahamat01@gmail.com>
2019-09-29 18:09:29 +0600

    project_work

---
+++ app/src/main/java/com/rahamat/myprojectcountryhistory/MainActivity.java
@@ -0,0 +1,45 @@
+package com.rahamat.myprojectcountryhistory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setIcon(R.drawable.ic_action_name);
+        alertDialogBuilder.setTitle("This is Opportinity ?????");
+        alertDialogBuilder.setMessage("What is went ?");
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+              finish();
+
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+    }
+}
